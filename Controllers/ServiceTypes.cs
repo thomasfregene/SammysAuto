af837e60d3b8a2f3c6b02ae1f581b2083ef95338
thomasfregene <fregzythomas@gmail.com>
2020-07-29 23:31:06 +0100

    Add Index and Create Views -ServiceTypesController

--- Controllers/ServiceTypes.cs
+++ Controllers/ServiceTypes.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SammysAuto.Data;
+using SammysAuto.Models;
 
 namespace SammysAuto.Controllers
 {
     public class ServiceTypes : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ServiceTypes(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET: ServiceType
         public IActionResult Index()
         {
+            return View(_db.ServiceTypes.ToList());
+        }
+
+        //GET: Services/Create
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //POST: Services/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ServiceType serviceType)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(serviceType);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceType);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
         }
     }
 }
---
+++ Data/Migrations/20200729205840_UpdateFieldInServiceType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SammysAuto.Data.Migrations
+{
+    public partial class UpdateFieldInServiceType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ServiceTypes",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ServiceTypes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
