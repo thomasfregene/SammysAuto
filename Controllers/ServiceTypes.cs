9875a33f483ebcbf129fae92d43af11cc254b076
thomasfregene <fregzythomas@gmail.com>
2020-07-30 00:02:46 +0100

    Added Details Action and View -ServiceTypeController

--- Controllers/ServiceTypes.cs
+++ Controllers/ServiceTypes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SammysAuto.Data;
 using SammysAuto.Models;
 
@@ -39,6 +40,22 @@ namespace SammysAuto.Controllers
                 _db.Add(serviceType);
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
+            }
+            return View(serviceType);
+        }
+
+        //Details: ServiceTypes/Details/1
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if(serviceType == null)
+            {
+                return NotFound();
             }
             return View(serviceType);
         }
