dde7a1a307eedd72bf5fb2301deeaee11c9f0096
thomasfregene <fregzythomas@gmail.com>
2020-08-04 00:34:45 +0100

    Added Index, Delete Actions and Views -ServicesController

--- Controllers/ServicesController.cs
+++ Controllers/ServicesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SammysAuto.Data;
+using SammysAuto.Models;
 using SammysAuto.ViewModel;
 
 namespace SammysAuto.Controllers
@@ -17,9 +18,23 @@ namespace SammysAuto.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
+        public IActionResult Index(int carId)
         {
-            return View();
+            var car = _db.Cars.FirstOrDefault(c => c.Id == carId);
+            var model = new CarAndServicesViewModel
+            {
+                CarId = car.Id,
+                Make = car.Make,
+                Model = car.Model,
+                Style = car.Style,
+                VIN = car.VIN,
+                Year = car.Year,
+                UserId = car.UserId,
+                ServiceTypesObj = _db.ServiceTypes.ToList(),
+                PastServicesObj = _db.Services.Where(s => s.CarId == carId).OrderByDescending(s => s.DateAdded)
+            };
+
+            return View(model);
         }
 
         //GET : Services/Create
@@ -70,6 +85,42 @@ namespace SammysAuto.Controllers
             };
             return View(newModel);
 
+        }
+
+        //DELETE GET
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _db.Services.Include(s => s.Car).Include(s => s.ServiceType)
+                .SingleOrDefaultAsync(m => m.Id == id);
+
+            if(service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        //POST DELETE
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Service model)
+        {
+            var serviceId = model.Id;
+            var carId = model.CarId;
+            var service = await _db.Services.SingleOrDefaultAsync(m => m.Id == serviceId);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            _db.Services.Remove(service);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Create), new { CarId = carId });
         }
 
         protected override void Dispose(bool disposing)
